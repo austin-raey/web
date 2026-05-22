c4e021b45809c441be1f70bbf3ac1a06f5c944ed
Austin Raey <24833719+austin-raey@users.noreply.github.com>
2026-05-21 20:38:16 -0400

    📦 package bump, font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,29 @@
 // @ts-check
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
 	build: {
 		inlineStylesheets: "always"
 	},
+	fonts: [
+		{
+			cssVariable: "--font",
+			name: "Schibsted Grotesk",
+			provider: fontProviders.google(),
+			styles: ["normal", "italic"],
+			weights: ["400 900"]
+		}
+	],
 	integrations: [mdx(), sitemap()],
-	markdown: {
-		shikiConfig: {
-			themes: {
-				dark: "catppuccin-macchiato",
-				light: "catppuccin-latte"
-			}
+	output: "static",
+	security: {
+		csp: {
+			algorithm: "SHA-512"
 		}
 	},
-	output: "static",
 	site: "https://raey.me",
 	trailingSlash: "never",
 	vite: {
